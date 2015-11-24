4b2a7dd9870910989e2d51555bb4a082388f4d38
Amit Apple <amitapl@outlook.com>
2015-11-24 09:30:36 -0800

    fix target

--- Story.Core/AssemblyInfo.cs
+++ Story.Core/AssemblyInfo.cs
@@ -1,4 +1,4 @@
 ﻿using System.Reflection;
 
-[assembly: AssemblyVersion("0.2.1")]
-[assembly: AssemblyFileVersion("0.2.1")]
+[assembly: AssemblyVersion("0.2.2")]
+[assembly: AssemblyFileVersion("0.2.2")]
